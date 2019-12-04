e62d1c0cd7cfc2db57d75502217549c5f318de97
kylewu1215 <47725981+kylewu1215@users.noreply.github.com>
2019-12-03 22:23:30 -0500

    mailer working

--- config/mailer.js
+++ config/mailer.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MAILGUN_USER: 'postmaster@sandbox60871f27b70849e6a53e89fd21655005.mailgun.org	',
+    MAILGUN_USER: 'postmaster@sandbox60871f27b70849e6a53e89fd21655005.mailgun.org',
     MAILGUN_PASS: 'fc1933f06feee33379cff8e906a7e686-f7910792-53a21ea8'
 };
